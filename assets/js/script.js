0d85f99db9a83865acbeecadd0090b985cbf36fb
James Capadocia <james.capadocia@gmail.com>
2021-09-05 15:42:17 -0500

    Had to run a separate fetch to obtain the UV index and append the data to the .html file. Main container now shows all appropriate information based on acceptance criteria.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,155 +1,119 @@
-$("#searchButton").click(function() {
+$("#searchButton").click(function () {
   city = $("#searchInput").val();
-  // fetchAPI();
 
   var checkArray = searchHistory.includes(city);
 
   if (checkArray == true) {
-      return;
+    return;
   } else {
-      searchHistory.push(city);
-      localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    searchHistory.push(city);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
-      var historyButton = $("<a>").attr({
-          class: "list-group-item list-group-item-action",
-          href: "#",
-
-      });
-      historyButton.text(city);
-      $("#searchList").append(historyButton);
-  };
+    var historyButton = $("<a>").attr({
+      class: "list-group-item list-group-item-action",
+      href: "#",
+    });
+    historyButton.text(city);
+    $("#searchList").append(historyButton);
+  }
 });
-$(".list-group-item").click(function() {
+$(".list-group-item").click(function () {
   city = $(this).text();
-  // fetchAPI();
 });
-
-
-
 
 var searchHistory = [];
 
 function getItems() {
-    var storedHistory = JSON.parse(localStorage.getItem("searchHistory"));
-    if (storedHistory !== null) {
-        searchHistory = storedHistory;
-    };
+  var storedHistory = JSON.parse(localStorage.getItem("searchHistory"));
+  if (storedHistory !== null) {
+    searchHistory = storedHistory;
+  }
 
-    for (i = 0; i < searchHistory.length; i++) {
-        if (i == 8) {
-            break;
-          }
-
-          historyButton = $("<a>").attr({
-            class: "list-group-item list-group-item-action",
-            href: "#"
-        });
-
-        historyButton.text(searchHistory[i]);
-        $("#searchList").append(historyButton);
+  for (i = 0; i < searchHistory.length; i++) {
+    if (i == 8) {
+      break;
     }
-};
+
+    historyButton = $("<a>").attr({
+      class: "list-group-item list-group-item-action",
+      href: "#",
+    });
+
+    historyButton.text(searchHistory[i]);
+    $("#searchList").append(historyButton);
+  }
+}
 
 getItems();
-
-
-
-        
-
-
-// var $searchButton = $("#searchButton");
-// var $searchInput = $("#searchInput");
-
-// function searchButtonHandler() {
-//   var searchInput = $searchInput.val();
-//   var apiKey = "3a150e01056da8ad0b1ee8083da97feb";
-//   var weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=${apiKey}`;
-
-//   $.ajax({
-//     url: weatherUrl,
-//     success: function(data) {
-//       console.log(data);
-
-//       var appendedDates = [];
-//       var appendedCount = 0;
-
-//       for (let i=0; i < data.list.length; i++) {
-//         var curr = data.list[i];
-//         var currDateText = curr.dt_txt;
-//         var currDate = currDateText.split(" ")[0];
-
-//         if (!appendedDates.includes(currDate)) {
-//           appendedDates.push(currDate);
-//           var temp = curr.main.temp;
-
-//           $("#searchlist").append(
-//             getPanelHTML(currDateText, temp)
-//           )
-//           appendedCount++;
-//           if (appendedCount > 4) {
-//             break;
-//           }
-//         }
-//       }
-//     }
-//   })
-// };
-
-// $searchButton.click(searchButtonHandler);
-
-
-
-
-// $("#searchButton").click(function citySearch() {
 
 var searchButton = $("#searchButton");
 
 searchButton.on("click", citySearch);
 
-function citySearch(e) {    
+function citySearch(e) {
   e.preventDefault();
-  console.log("it works") 
+  console.log("it works");
 
-var key = "3a150e01056da8ad0b1ee8083da97feb";
-var city = $("#searchInput").val();
-var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
+  var key = "3a150e01056da8ad0b1ee8083da97feb";
+  var city = $("#searchInput").val();
+  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
 
-console.log(url)
+  console.log(url);
 
-// fetch(url, {
-//     method: 'GET',
-//   })
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   })
-// };
+  // fetch(url, {
+  //     method: 'GET',
+  //   })
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     console.log(data);
+  //   })
+  // };
 
-
-fetch(url, {
-  method: 'GET',
+  fetch(url, {
+    method: "GET",
   })
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      var cityChoice = $(".cityChoice").append("<div>").addClass("currentCity");
+      cityChoice.empty();
 
-    var cityChoice = $(".cityChoice").append("<div>").addClass("currentCity");
-    cityChoice.empty();
-      
-    var currentName = cityChoice.append("<p>");
-    cityChoice.append(currentName);
+      var currentName = cityChoice.append("<p>");
+      cityChoice.append(currentName);
 
-    var timeUTC = new Date(response.dt * 1000);
-    currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
-    currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+      var timeUTC = new Date(response.dt * 1000);
+      currentName.append(
+        response.name + " " + timeUTC.toLocaleDateString("en-US")
+      );
+      currentName.append(
+        `<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`
+      );
 
-    var currentWeather = currentName.append("<p>");
-    currentName.append(currentWeather);
-    currentWeather.append("<p>" + "Temperature: " + response.main.temp + "</p>");
-    currentWeather.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
-    currentWeather.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
-  })
-};
+      var currentWeather = currentName.append("<p>");
+      currentName.append(currentWeather);
+      currentWeather.append(
+        "<p>" + "Temperature: " + response.main.temp + " F" + "</p>"
+      );
+      currentWeather.append(
+        "<p>" + "Humidity: " + response.main.humidity + "%" + "</p>"
+      );
+      currentWeather.append(
+        "<p>" + "Wind Speed: " + response.wind.speed + " mph" + "</p>"
+      );
+
+      var uvURL = `https://api.openweathermap.org/data/2.5/uvi?q=&lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${key}`;
+
+      fetch(uvURL, {
+        method: "GET",
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          currentWeather.append("<p>" + "UV Index: " + response.value + "</p>");
+        });
+    });
+}
