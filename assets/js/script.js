99b1d0649131959f29b0e7af34bbaa59b65dea93
James Capadocia <james.capadocia@gmail.com>
2021-09-06 12:30:45 -0500

    Added a class 'appendAll' to all appended elements in 5 Day Forecast and applied .remove() to the beginning of the function to ensure all appended items are cleared prior to a new user search. Added comments to .js to explain the flow of code. Adjusted minor .css stylings.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+// Variables to be used in overall code
 var searchButton = $("#searchButton");
 var key = "3a150e01056da8ad0b1ee8083da97feb";
 var city = $("#searchInput").val();
 
+// Begin function to save user input in local storage
 $("#searchButton").click(function () {
   city = $("#searchInput").val();
 
@@ -13,31 +15,40 @@ $("#searchButton").click(function () {
     searchHistory.push(city);
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
 
+    // Creates list items as links for locally stored data as the output in Search History
+    // Sets output immediately when a city is searched
     var historyButton = $("<a>").attr({
       class: "list-group-item list-group-item-action",
       href: "#",
     });
+
     historyButton.text(city);
     $("#searchList").append(historyButton);
   }
 });
+
+// Eventlistener for any list item that is clicked in Search History
 $(".list-group-item").click(function () {
   city = $(this).text();
 });
 
 var searchHistory = [];
 
+// Begin function to retrieve locally stored data and have it persist in the Search History
 function getItems() {
   var storedHistory = JSON.parse(localStorage.getItem("searchHistory"));
   if (storedHistory !== null) {
     searchHistory = storedHistory;
   }
 
+  // For loop that will list a maximum of 8 items in Search History
   for (i = 0; i < searchHistory.length; i++) {
     if (i == 8) {
       break;
     }
 
+    // Creates list items as links for locally stored data as the output in Search History
+    // Gets locally stored data for it to persist
     historyButton = $("<a>").attr({
       class: "list-group-item list-group-item-action",
       href: "#",
@@ -46,25 +57,23 @@ function getItems() {
     historyButton.text(searchHistory[i]);
     $("#searchList").append(historyButton);
   }
-}
+};
 
 getItems();
 
-
-
+// Function to call API fetching functions
 function callFunctions(e) {
-  // location.reload(true);
   citySearch(e);
   fiveDay(e);
 };
 
+// Function to fetch all API data for the city searched via user input
+// Used for Current Weather
 function citySearch(e) {
   e.preventDefault();
-  console.log("it works");
 
+  // Begin API fetch and appending of data retrieved
   var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
-
-  console.log(url);
 
   fetch(url, {
     method: "GET",
@@ -73,7 +82,7 @@ function citySearch(e) {
       return response.json();
     })
     .then(function (response) {
-      var cityChoice = $(".cityChoice").append("<div>").addClass("currentCity");
+      var cityChoice = $(".cityInfo").append("<div>");
       cityChoice.empty();
 
       var currentName = cityChoice.append("<p>");
@@ -89,6 +98,7 @@ function citySearch(e) {
       currentWeather.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
       currentWeather.append("<p>" + "Wind: " + response.wind.speed + " mph" + "</p>");
 
+      // Begin API fetch and appending for UV index specifically
       var uvURL = `https://api.openweathermap.org/data/2.5/uvi?q=&lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${key}`;
 
       fetch(uvURL, {
@@ -98,31 +108,42 @@ function citySearch(e) {
           return response.json();
         })
         .then(function (response) {
-          currentWeather.append("<span>" + "UV Index: " + response.value + "</span>");
+          currentWeather.append(
+            "<span>" + "UV Index: " + response.value + "</span>"
+          );
+          // Contiditional statement to add a colored border around UV index determined by the value returned
           if (response.value <= 2) {
             $("span").addClass("borderGreen");
-          };
+          }
           if (response.value > 2 && response.value <= 5) {
             $("span").addClass("borderYellow");
-          };
+          }
           if (response.value > 5) {
             $("span").addClass("borderRed");
-          };
-        });
-    });
-  }
+          }
+        })
+    })
+};
 
+// Function to fetch all API data for the city searched via user input
+// Used for 5 Day Forecast
 function fiveDay(e) {
+  var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key + "&units=imperial";
   e.preventDefault();
-  $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key + "&units=imperial",
-    method: "GET"
+
+  // Begin API fetch and appending of data retrieved
+  fetch(urlFiveDay, {
+    method: "GET",
   })
     .then(function (response) {
-      console.log("this is response", response);
-      for (i = 0; i < 5; i++) {
+      return response.json();
+    })
+    .then(function (response) {
+      $(".appendedAll").remove();
 
-        var newItem = $("<div>").attr("class", "col-sm-8 bg-primary text-white rounded");
+      // For loop to create and append data retrieved for the next consecutive 5 days in separate <divs>
+      for (i = 0; i < 5; i++) {
+        var newItem = $("<div>").attr("class", "col-sm-7 bg-primary text-white rounded appendedAll");
         $(".weatherContent1").append(newItem);
 
         var date = new Date(response.list[i * 8].dt * 1000);
@@ -142,44 +163,7 @@ function fiveDay(e) {
         newItem.append("<p>" + ("Wind: " + wind + " mph") + "<p>");
       }
     })
-}
+};
 
-
-// function fiveDay(e) {
-//   e.preventDefault();
-//   $.ajax({
-//     url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key + "&units=imperial",
-//     method: "GET"
-//   })
-//     .then(function (response) {
-//       console.log("this is response", response);
-//       for (i = 0; i < 5; i++) {
-        
-//         var newItem = $("<div>");
-//         $(".fiveDay").append(newItem);
-
-//       // var date = new Date(response.list.dt * 1000);
-//       // var dateInfo = $("#date");
-//       // dateInfo.text(date.toLocaleDateString());
-
-//       // var iconCode = response.list.weather[0].icon;
-//       // var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
-//       // var icon = $("#icon")
-//       // icon.html($("<img>").attr("src", iconURL));
-
-//       var temp = response.list[i].main.temp;
-//       var tempInfo = $("#temperature")
-//       tempInfo.text("Temp: " + temp + " F");
-
-//       var humidity = response.list[i].main.humidity;
-//       var humidityInfo = $("#humidity")
-//       humidityInfo.text("Humidity: " + humidity);
-
-//       var wind = response.list[i].wind.speed;
-//       var windInfo = $("#wind")
-//       windInfo.text("Wind: " + wind + " mph");
-//       }
-//     })
-// }
-
+// Begin event listener to call all functions needed for code
 searchButton.on("click", callFunctions);
